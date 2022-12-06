290517375a1e7c213afcac96b2770b1d93377c43
calin <calin@dumitru.it>
2022-12-06 13:17:33 +0100

    day1 part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,20 @@
-import kotlin.math.max
-
 fun main() {
     val part1 = Implementation("Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?",
         24000) { lines ->
-        val joinToString = lines.joinToString("+")
-        println(joinToString)
-        val maxCalories = joinToString.split("++")
-            .filter { it.isNotBlank() }.maxOf { elf -> elf.split("+").sumOf { it.toInt() } }
-
-        println("calories: $maxCalories")
-        return@Implementation maxCalories
+        return@Implementation computeCaloriesByElf(lines).max()
     }
-    OhHappyDay(1, part1).checkResults()
+    val part2 = Implementation("Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?",
+        45000) { lines ->
+        return@Implementation computeCaloriesByElf(lines).sortedDescending().take(3).sum()
+    }
+    OhHappyDay(1, part1, part2).checkResults()
+}
+
+fun computeCaloriesByElf(lines: List<String>): List<Int> {
+    //c1elf1+c2elf1++c1elf2+c2elf2+c3elf2++c1elf3...
+     val allCalories = lines.joinToString("+")
+    return allCalories.split("++")
+        .filter { calorieValuesConcatenatedByPlus -> calorieValuesConcatenatedByPlus.isNotBlank() }
+        .map{elf -> elf.split("+")
+            .sumOf { calorieValue -> calorieValue.toInt() }}
 }
