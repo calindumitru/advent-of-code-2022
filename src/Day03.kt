e31a86b7f662465041f0245500de4d31e463d695
calin <calin@dumitru.it>
2022-12-06 22:38:17 +0100

    day3 readability

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,26 +1,24 @@
-import java.util.*
-
 fun main() {
     val part1 = Implementation("Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?",
         157) { lines ->
         val sum = lines.map { it.chunked(it.length / 2) }
             .map { twoChunks -> (twoChunks.get(0).toCharSet() to twoChunks.get(1).toCharSet()) }
             .map { (a, b) -> (a.intersect(b)) }
-            .flatten().sumOf { calculate(it) }
+            .flatten().sumOf { calculatePriority(it) }
         return@Implementation sum
     }
     val part2 = Implementation("Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of those item types?",
         70) { lines ->
         val sum = lines.chunked(3)
             .map { it.get(0).toCharSet().intersect(it.get(1).toCharSet()).intersect(it.get(2).toCharSet()) }
-            .flatten().sumOf { calculate(it) }
+            .flatten().sumOf { calculatePriority(it) }
         return@Implementation sum
     }
 
     OhHappyDay(3, part1, part2).checkResults()
 }
 
-fun calculate(it: Char): Int {
+fun calculatePriority(it: Char): Int {
     return if (it.isLowerCase()) {
         pos(it)
     } else {
